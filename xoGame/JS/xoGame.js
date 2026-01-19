c55866f110b2eec4665d6178f789320a8e8797c0
Yaroslav Domb <yaroslav.domb@gmail.com>
2026-01-19 11:48:15 +0200

    Adding win condition and design

--- xoGame/JS/xoGame.js
+++ xoGame/JS/xoGame.js
@@ -1,15 +1,17 @@
 const possibleWinStates = [
-    [0,1,2],[3,4,5],[6,7,8]     //horizontal
+    [0,1,2],[3,4,5],[6,7,8],    //horizontal
     [0,3,6],[1,4,7],[2,5,8],    //vertical
     [0,4,8],[2,4,6]             //diagonals
 ];
 
 const SYGN_X = "X";
 const SYGN_O = "O";
+const BOARD_SIZE = 9;
 let isTurnToPlayO = true;
+let winCombination = [];
 
 const grid = document.querySelector(".playBoard");
-for (let i = 0 ; i < 9 ; i++) {
+for (let i = 0 ; i < BOARD_SIZE ; i++) {
     const divElement = document.createElement("div");
     divElement.addEventListener("click", function(event) {
 
@@ -19,13 +21,39 @@ for (let i = 0 ; i < 9 ; i++) {
 
         if (isTurnToPlayO) {
             event.target.innerText = SYGN_O;
+            event.target.classList.add("takenByO");      
         } else {
             event.target.innerText = SYGN_X;
+            event.target.classList.add("takenByX");
         }
+        
         isTurnToPlayO = !isTurnToPlayO;
-
+        if (isGameFinished()) {
+            markWinCombination();
+        }
     });
 
     divElement.className = "cell";
     grid.appendChild(divElement);
+}
+
+function isGameFinished() {
+    
+    const possibleWinSygn = isTurnToPlayO ? SYGN_X : SYGN_O;
+    const allCellsArr = document.querySelectorAll(".cell");
+    for (const currentState of possibleWinStates) {
+        if (currentState.every(i => allCellsArr[i].innerText === possibleWinSygn)) {
+            winCombination = currentState;
+            alert (possibleWinSygn + " WINS");
+            return true;
+        }
+    }
+    return false;
+}
+
+function markWinCombination() {
+    const allCellsArr = document.querySelectorAll(".cell");
+    winCombination.forEach(i => {
+        allCellsArr[i].classList.add("winner");
+    });
 }
