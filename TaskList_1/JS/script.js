bf44ca8db7f791467ee802c1e6d821f321e89627
Yaroslav Domb <yaroslav.domb@gmail.com>
2026-02-08 22:18:40 +0200

    update taskList_v1 first normal view

--- TaskList_1/JS/script.js
+++ TaskList_1/JS/script.js
@@ -1,12 +1,11 @@
 const ul = document.querySelector("ul");
 const tasks = [
-    { subject: 'לקנות חלב', checked: true },
-    { subject: 'להכין קפה', checked: false },
-    { subject: 'לתרגל 8 שעות בשבוע', checked: false },
+    { subject: "Check homework", checked: true },
+    { subject: "Update timetable", checked: false },
+    { subject: "Sleep at least 8H", checked: false },
 ];
 
 /**
- * יצירת אלמנט ב-HTML
  * @param {*} obj { subject: string; checked: boolean }
  */
 function createTask(obj, i) {
@@ -15,24 +14,20 @@ function createTask(obj, i) {
     const div = document.createElement("div");
     const remove = document.createElement("span");
 
-    // אם המשימה בוצעה, מסמנים אותה בהתאם
     if (obj.checked) {
         li.classList.add("checked");
         input.checked = true;
     }
 
-    // מאפשר לערוך
     div.contentEditable = true;
 
     input.type = "checkbox";
-    // כל שינוי בסטטוס המשימה
     input.addEventListener("change", ev => {
         obj.checked = ev.target.checked;
         li.classList.toggle("checked");
     });
 
     div.innerHTML = obj.subject;
-    // מעקב אחר שינויים בתוכן המשימה
     div.addEventListener("input", ev => {
         obj.subject = ev.target.innerText;
     });
@@ -49,13 +44,10 @@ function createTask(obj, i) {
                 ul.children[i - 1].querySelector('div').focus();
             }
         } else if (key == 'Enter') {
-            // אם נמצאים במשימה האחרונה, שזה יוסיף משימה חדשה
         } else if (key == 'Backspace') {
-            // אם אין תוכן במשימה, שימחק אותה
         }
     });
 
-    // לחצן מחיקה
     remove.innerText = 'X';
     remove.className = "remove";
     remove.addEventListener("click", () => {
@@ -80,5 +72,4 @@ function newTask() {
     createTask(obj, tasks.length - 1);
 }
 
-// מפעיל את הפונקציה של הוספת משימה על כל אובייקט במערך
 tasks.forEach(createTask);
